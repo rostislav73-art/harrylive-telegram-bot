77159a5b09f1af7830e209c32327520c3d9d23e4
rostislav73-art <rostislav.d73@gmail.com>
2025-04-04 18:25:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import telebot
 import openai
-
 bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
 openai.api_key = os.getenv("OPENAI_KEY")
-
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     response = openai.ChatCompletion.create(
@@ -13,5 +11,4 @@ def handle_message(message):
     )
     reply = response.choices[0].message.content
     bot.send_message(message.chat.id, reply)
-
 bot.polling()
