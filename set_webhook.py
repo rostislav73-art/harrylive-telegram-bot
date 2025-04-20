6e196613d1e99426c6467c9de7166602315c3bbd
Rostislav <your-email@example.com>
2025-04-20 19:13:32 +0300

    Resolved conflict in requirements.txt

---
+++ set_webhook.py
@@ -0,0 +1,17 @@
+
+import requests
+
+BOT_TOKEN = "8141162527:AAG0tqiTHkL7GX5jEFwI0F1cnyFqAFqxX3I"
+
+RAILWAY_URL = "https://web-production-f7800.up.railway.app"
+
+# 👉 Генерираме URL за webhook
+webhook_url = f"{RAILWAY_URL}/{BOT_TOKEN}"
+telegram_api_url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
+
+# 🚀 Изпращаме заявка към Telegram
+response = requests.get(telegram_api_url, params={"url": webhook_url})
+
+print("===> Отговор от Telegram:")
+print("Статус код:", response.status_code)
+print("Съобщение:", response.text)
